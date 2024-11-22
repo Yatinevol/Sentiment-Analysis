3cb6bb51ac46e66519ead5a4c6d9256162337543
Yatin_Singh <140312835+Yatinevol@users.noreply.github.com>
2024-11-22 16:32:52 +0530

    Update app.py

--- Sentiment-Analysis-ML-Flask-App/app.py
+++ Sentiment-Analysis-ML-Flask-App/app.py
@@ -39,4 +39,5 @@ def my_form_post():
     return render_template('form.html', final=compound, text1=text_final,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
 
 if __name__ == "__main__":
-    app.run(debug=False, host="127.0.0.1", port=5002, threaded=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(debug=False, host="0.0.0.0", port=port)
